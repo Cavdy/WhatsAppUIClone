050e634b73bfadbbe8280e87faad56e5078812b0
Cavdy <cavdyofficials@gmail.com>
2018-10-16 01:16:09 +0100

    initial commit

---
+++ lib/whatsapp_home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class WhatsAppHome extends StatefulWidget {
+  @override
+  _WhatsAppHomeState createState() => _WhatsAppHomeState();
+}
+
+class _WhatsAppHomeState extends State<WhatsAppHome> with SingleTickerProviderStateMixin {
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = new TabController(length: 4, vsync: this, initialIndex: 1 );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("WhatsApp"),
+        elevation: 0.7,
+        bottom: new TabBar(controller: _tabController, indicatorColor: Colors.white, tabs: <Widget>[
+          new Tab(icon: new Icon(Icons.camera_alt),),
+          new Tab(text: "CHATS",),
+          new Tab(text: "STATUS",),
+          new Tab(text: "CALLS",)
+        ],),
+      ),
+      body: new TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          new CameraScreen(),
+          new ChatScreen(),
+          new StatusScreen(),
+          new CallScreen(),
+        ],
+      ),
+      floatingActionButton: new FloatingActionButton(onPressed: (){print("Open Chats");}, backgroundColor: Theme.of(context).accentColor, child: new Icon(Icons.message),),
+    );
+  }
+}
